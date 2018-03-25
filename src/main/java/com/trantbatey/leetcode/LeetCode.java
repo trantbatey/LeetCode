7ad50a5368b3d02c8ac59fdfae06b4e7602284e0
TrantBatey <trantbatey@gmail.com>
2018-03-24 20:32:15 -0500

    Initial commit

---
+++ src/main/java/com/trantbatey/leetcode/LeetCode.java
@@ -0,0 +1,23 @@
+package com.trantbatey.leetcode;
+
+/**
+ * This is a master module for a group of programs written 
+ * to solve LeetCode programming exercises. 
+ * 
+ * @author Trant
+ *
+ */
+import static java.lang.System.out;
+
+public class LeetCode {
+	
+	/**
+	 * The main methods starts all of the exercise program.
+	 * 
+	 * @param args 
+	 * 		Input arguments to main currently not used
+	 */
+	public static void main(String[] args) {
+		out.println("LeetCode Main");
+	}
+}
